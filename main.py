c26abeb5a5b764c9203088bdee8cd917a421f1ab
RonanRibouletPython <newsletterpython13@gmail.com>
2026-03-01 12:40:12 +0000

    Adding image preprocessing

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from poc.tesseract import POCTesseract
 
 ocr = POCTesseract()
 
-image_path = "images/ausias.png"
+# image_path = "poc/output/01_normalized.png"
+image_path = "poc/output/05_ocr_ready.png"
 text = ocr.get_image_text(image_path=image_path, language="cat")
 print("Extracted text:")
 print(text)
