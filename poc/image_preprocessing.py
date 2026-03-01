84bbb6e8250a62b9c1e4255b26e7adbd57f63ac6
RonanRibouletPython <newsletterpython13@gmail.com>
2026-03-01 15:50:07 +0000

    test phase done

--- poc/image_preprocessing.py
+++ poc/image_preprocessing.py
@@ -263,7 +263,7 @@ class ImagePreprocessing:
 if __name__ == "__main__":
     preprocessor = ImagePreprocessing()
 
-    image_path = "../images/test.jpeg"
+    image_path = "../images/test.png"
     preprocessor.load_image(image_path)
 
     print("Original image info:")
